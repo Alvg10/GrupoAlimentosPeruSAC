73eea33c72d9e0d7573808cee8897cc6ebe690dc
dev.ega <dev.ega.email>
2025-11-11 17:47:29 -0500

    1er push

---
+++ alimentosperu/src/main/java/com/grupo/alimentos/peru/alimentosperu/AlimentosperuApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.alimentos.peru.alimentosperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentosperuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentosperuApplication.class, args);
+	}
+
+}
---
+++ alimentosperu/src/test/java/com/grupo/alimentos/peru/alimentosperu/AlimentosperuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.alimentos.peru.alimentosperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentosperuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
