4732aba62bbc763498b6e2fb0e4e06510a199e0f
dev.ega <114100372+Alvg10@users.noreply.github.com>
2025-11-18 12:22:21 -0500

    First

---
+++ Proyect-GrupoAlimentosPeruSAC/src/main/java/com/grupo/alimentos/peru/GrupoAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.alimentos.peru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoAlimentosApplication.class, args);
+	}
+
+}
---
+++ Proyect-GrupoAlimentosPeruSAC/src/test/java/com/grupo/alimentos/peru/GrupoAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.alimentos.peru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
